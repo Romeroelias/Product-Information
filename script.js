dbcd74f80ebf495d78433998b1dad336fd2a16cc
Elias Romero <eliasrmr2004@gmail.com>
2023-12-13 09:23:55 -0600

    added html, css and javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+// Variables
+const userInputContainer = document.getElementById("userinput-container");
+
+//POPULARITY FUNCTION//
+popularity();
+
+function popularity(){
+
+    let popular = "Popular";
+    let NotAsPopular = "Not As Popular";
+    let NotPopular = "Not popular";
+    if (sales >= 100){
+        return popular;
+    }
+
+    else if(sales < 50){
+        return NotPopular;
+    }
+    
+    else{
+        return NotAsPopular;
+    }
+}
+
+//SUBMIT FUNCTION//
+document.getElementById('submitButton').onclick = function(){
+//input variables//
+let item = document.getElementById('item').value;
+let sales = document.getElementById('sales').value;
+//creating new HTML elements//
+let newDiv = document.createElement('div');
+newDiv.classList.add('newdiv-css');
+userInputContainer.append(newDiv);
+
+newDiv.innerHTML = `${item} <hr> ${popularity()}`;
+}
